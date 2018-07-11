57f9167ee38325d71cf8aeda1e3b80c0843b047b
Thomas Pope <tpope0928@gmail.com>
2018-07-11 20:25:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,3 @@
 def position_taken?(board, location)
-  !(board[location] = nil? || board[location] == " " || board[location] == ""
+  !(board[location].nil? || board[location] == " " || board[location] == "")
 end
